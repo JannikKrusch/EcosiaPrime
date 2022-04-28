01de5f24ed75d4adf93d4cf94c89fae40aeff52e
Paul Maibach <pma@indeca.de>
2022-04-28 16:37:52 +0200

    Added comments for methods, shortened methods

--- src/EcosiaPrime.Gui/ExtensionMethods/InvokeExtensionMethods.cs
+++ src/EcosiaPrime.Gui/ExtensionMethods/InvokeExtensionMethods.cs
@@ -4,6 +4,11 @@ namespace EcosiaPrime.Gui.ExtensionMethods
 {
     public static class InvokeExtensionMethods
     {
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, den Text aus einer ComboBox rauszuholen, welche dann returned wird
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <returns></returns>
         public static string InvokeComboBox(this ComboBox comboBox)
         {
             var text = "";
@@ -14,6 +19,11 @@ namespace EcosiaPrime.Gui.ExtensionMethods
             return text;
         }
 
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, den Text in der dementsprechenden ComboBox mit dem des input Parameters zu ersetzen
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <param name="input"></param>
         public static void InvokeComboBox(this ComboBox comboBox, string input)
         {
             comboBox.Invoke(new Action(() =>
@@ -22,6 +32,11 @@ namespace EcosiaPrime.Gui.ExtensionMethods
             }));
         }
 
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, den Text in der dementsprechenden TextBox mit dem des input Parameters zu ersetzen
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <param name="input"></param>
         public static void InvokeTextBox(this TextBox textBox, string input)
         {
             textBox.Invoke(new Action(() =>
@@ -30,15 +45,24 @@ namespace EcosiaPrime.Gui.ExtensionMethods
             }));
         }
 
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, einer Textbox ein Array vom Typ string zu übergeben
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="lines"></param>
         public static void InvokeResponseTextBox(this TextBox response, IEnumerable<string> lines)
         {
             response.Invoke(new Action(() =>
             {
-                var x = lines.ToArray();
-                response.Lines = x;
+                response.Lines = lines.ToArray();
             }));
         }
 
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, den Text in dem dementsprechenden DateTimePicker mit dem des input Parameters zu ersetzen
+        /// </summary>
+        /// <param name="dateTimePicker"></param>
+        /// <param name="input"></param>
         public static void InvokeDateTimePicker(this DateTimePicker dateTimePicker, string input)
         {
             dateTimePicker.Invoke(new Action(() =>
@@ -47,17 +71,27 @@ namespace EcosiaPrime.Gui.ExtensionMethods
             }));
         }
 
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, das Datum aus dem dementsprechenden DateTimePicker zu holen und zu returnen
+        /// </summary>
+        /// <param name="dateTimePicker"></param>
+        /// <returns></returns>
         public static DateTime GetDateTime(this DateTimePicker dateTimePicker)
         {
-            DateTime s = default;
+            DateTime datetime = default;
             dateTimePicker.Invoke(new Action(() =>
             {
-                s = dateTimePicker.Value.Date;
+                datetime = dateTimePicker.Value.Date;
             }));
 
-            return s;
+            return datetime;
         }
 
+        /// <summary>
+        /// Ein Thread(GUI Thread) bekommt die Aufgabe, einer Listview den input Parameter vom Typ string Array hinzuzufügen
+        /// </summary>
+        /// <param name="listView"></param>
+        /// <param name="input"></param>
         public static void InvokeListView(this ListView listView, string[] input)
         {
             var listViewItem = new ListViewItem(input);
@@ -67,6 +101,12 @@ namespace EcosiaPrime.Gui.ExtensionMethods
             }));
         }
 
+        /// <summary>
+        /// Es wird geprüft ob das Objekt oder die Id null sind, wenn nicht null -> werden alle werte in den Feldern als string in ein Array gepackt und das Array dann returned
+        /// wenn null -> neuer string Array wird erstellt und returned
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         private static string[] GetFilledRow(this Client client)
         {
             if (client != null && client.Id != null)
@@ -81,11 +121,21 @@ namespace EcosiaPrime.Gui.ExtensionMethods
             return new string[] { };
         }
 
+        /// <summary>
+        /// Es wird für jedes Element im IEnumerable die InvokelistView Methode ausgeführt, was dann die ListView mit den Attributen der Client-Objekte ausfüllt
+        /// </summary>
+        /// <param name="listView"></param>
+        /// <param name="clients"></param>
         public static void FillListView(this ListView listView, IEnumerable<Client> clients)
         {
             clients.ToList().ForEach(client => InvokeListView(listView, GetFilledRow(client)));
         }
 
+        /// <summary>
+        /// Der client Parameterwert wird wieder als Parameterwert benutzt für die GetFilledRow Methode, die dann in einer ListView eine Reihe ausfüllt mit den Attributen des Objekts
+        /// </summary>
+        /// <param name="listView"></param>
+        /// <param name="client"></param>
         public static void FillListView(this ListView listView, Client client)
         {
             listView.InvokeListView(GetFilledRow(client));
--- src/EcosiaPrime.Gui/IGuiService.cs
+++ src/EcosiaPrime.Gui/IGuiService.cs
@@ -1,17 +1,13 @@
-﻿namespace EcosiaPrime.Gui
+﻿
+namespace EcosiaPrime.Gui
 {
     public interface IGuiService
     {
         Task<bool> CreateClientAsync(TextBox response, TextBox id, TextBox firstName, TextBox lastName, TextBox email, TextBox password, TextBox country, TextBox state, TextBox postCode, TextBox city, TextBox street, TextBox houseNumber, DateTimePicker startDate, DateTimePicker endDate, ComboBox paymentMethod, ComboBox subscriptionType);
-
         Task<bool> DeleteClientAsync(TextBox response, TextBox id, TextBox firstName, TextBox lastName, TextBox email, TextBox password, TextBox country, TextBox state, TextBox postCode, TextBox city, TextBox street, TextBox houseNumber, DateTimePicker startDate, DateTimePicker endDate, ComboBox paymentMethod, ComboBox subscriptionType);
-
         Task SearchAttributesAsync(ListView table, TextBox response, string searchForString, string searchStringPrimary, string searchStringSecondary);
-
         Task SearchFunctionAsync(ListView table, ComboBox filter, TextBox response, TextBox id, TextBox firstName, TextBox lastName, TextBox email, TextBox password, TextBox country, TextBox state, TextBox postCode, TextBox city, TextBox street, TextBox houseNumber, DateTimePicker startDate, DateTimePicker endDate, ComboBox paymentMethod, ComboBox subscriptionType);
-
         Task ShowClientsAsync(ComboBox filter, ListView table, string id);
-
         Task<bool> UpdateClientAsync(TextBox response, TextBox id, TextBox firstName, TextBox lastName, TextBox email, TextBox password, TextBox country, TextBox state, TextBox postCode, TextBox city, TextBox street, TextBox houseNumber, DateTimePicker startDate, DateTimePicker endDate, ComboBox paymentMethod, ComboBox subscriptionType);
     }
 }
